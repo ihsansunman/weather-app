cdb09c456ce516230d757746fe6c309032a83f47
ihsansunman <ihsansunman@yandex.com>
2022-03-18 12:17:05 +0300

    Set theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Input,
+  InputGroup,
+  InputGroupText,
+  Button,
+  Card,
+  CardGroup,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+} from "reactstrap";
+import "./App.scss";
+import Time from "./components/CurrentTime"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather-app">
+      <h1 className="head-name">
+        Weather APP <h1 className="time"><Time/></h1>
+      </h1>
+
+      <InputGroup className="country-input">
+        <InputGroupText>Country</InputGroupText>
+        <Input />
+        <Button>Search</Button>
+      </InputGroup>
+
+      <CardGroup>
+        <Card body>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card body>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card body>
+          <CardImg
+            alt="Card image cap"
+            src="https://picsum.photos/318/180"
+            top
+            width="100%"
+          />
+          <CardBody>
+            <CardTitle tag="h5">Card title</CardTitle>
+            <CardSubtitle className="mb-2 text-muted" tag="h6">
+              Card subtitle
+            </CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
     </div>
   );
 }
